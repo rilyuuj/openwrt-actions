5c59360ee53d54eaaede3408b00387e8f9738fba
rilyuuj <ashorez@gmail.com>
2020-04-08 16:16:15 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -9,8 +9,8 @@
 sed -i 's/192.168.1.1/192.168.123.254/g' package/base-files/files/bin/config_generate
 # OpenClash for Luci
 git clone https://github.com/vernesong/OpenClash package/OpenClash
-./scripts/feeds update OpenClash
-./scripts/feeds install OpenClash
+./scripts/feeds update luci-app-openclash
+./scripts/feeds install luci-app-openclash
 # Kcptun client for Luci
 git clone https://github.com/kuoruan/luci-app-kcptun.git package/luci-app-kcptun
 ./scripts/feeds update luci-app-kcptun
